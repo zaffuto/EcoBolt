8f9e0fe725d70f45ea96e70f0d4ae7f32204d692
Smarter BOLT <hola@smarterbot.cl>
2024-12-10 18:28:02 -0300

    Add EcoBolt project contents

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata = {
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
   const cartId = (await cookies()).get('cartId')?.value;
-  const cart = cartId ? getCart(cartId) : undefined;
+  const cart = cartId ? getCart(cartId) : Promise.resolve(undefined);
 
   return (
     <html lang="en" className={GeistSans.variable}>
@@ -49,7 +49,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
       </head>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <ErrorBoundary>
-          <CartProvider cart={cart}>
+          <CartProvider cartPromise={cart}>
             <Navbar />
             <WelcomeToast />
             <Toaster richColors />
